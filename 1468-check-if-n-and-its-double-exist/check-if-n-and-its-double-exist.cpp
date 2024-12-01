8380cef5978c740ffabe4a9dd4e7110f95c7b56f
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-12-01 13:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (24.36%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
-    bool checkIfExist(vector<int>& arr) {
-        unordered_set<int>mpp;
-        for(int num:arr){
-            if(mpp.count(num*2) || (num%2==0 && mpp.count(num/2))){
-                return true;
+    int binarySearch(vector<int>& arr,int t){
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(arr[mid]==t){
+                return mid;
+            }else if(arr[mid]>t){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
-            mpp.insert(num);
+        }
+        return -1;
+    }
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        for(int i=0;i<arr.size();i++){
+            int target=2*arr[i];
+            int idx=binarySearch(arr,target);
+            if(idx>=0 && idx!=i){return true;}
         }
         return false;
     }
