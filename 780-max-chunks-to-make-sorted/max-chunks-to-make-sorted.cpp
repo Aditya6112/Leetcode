70fa8c9b59f813f4a44f8a040fa3ae6c5c22e134
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-12-20 02:11:52 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (7.22%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        vector<int>prefixMax,suffixMin;
-        int maxi=INT_MIN;
-        int mini=INT_MAX;
-        for(int i=0;i<arr.size();i++){
-            maxi=max(maxi,arr[i]);
-            prefixMax.push_back(maxi);
-        }
-        for(int i=arr.size()-1;i>=0;i--){
-            mini=min(mini,arr[i]);
-            suffixMin.push_back(mini);
-        }
-        reverse(suffixMin.begin(),suffixMin.end());
-
         int chunk=0;
+        int cummulative_sum=0;
+        int original_sum=0;
         for(int i=0;i<arr.size();i++){
-            int preMax=(i-1)>=0 ? prefixMax[i-1] : -1;
-            int suffMin=suffixMin[i];
-            if(preMax<suffMin)chunk++;
+            cummulative_sum+=arr[i];
+            original_sum+=i;
+            if(original_sum==cummulative_sum)chunk++;
         }
         return chunk;
     }
