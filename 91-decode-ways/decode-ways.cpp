c2f99f03f36318bfffc5030130fd4ca48296b4eb
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-12-24 15:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (45.30%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -13,10 +13,27 @@ public:
         }
         return dp[idx]=result;
     }
+    int bottomUp(string s,int n){
+        vector<int> dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+            }else{
+                dp[i]=dp[i+1];
+                if(i+1<n){
+                    if((s[i]=='1') || (s[i]=='2' && s[i+1]<='6')){
+                        dp[i]+=dp[i+2];
+                    }
+                }
+            }
+        }
+        return dp[0];
+    }
     int numDecodings(string s) {
         int n=s.size();
         int cnt=0;
         vector<int>dp(n+1,-1);
-        return solve(s,0,n,dp);
+        return bottomUp(s,n);
     }
 };
