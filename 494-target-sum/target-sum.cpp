026f09836cd6b0c59df6b1d534bd933669a5641b
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2024-12-27 16:08:30 +0530

    Time: 15 ms (60.04%) | Memory: 25.7 MB (26.32%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int idx,int target,int sum){
+    int solve(vector<int>& nums,int idx,int target,int sum,vector<vector<int>>& dp,int total){
         if(idx>=nums.size()){
             if(sum==target)return 1;
             else return 0;
         }
-        int plus=solve(nums,idx+1,target,sum+nums[idx]);
-        int minus=solve(nums,idx+1,target,sum-nums[idx]);
-        return plus+minus;
+        if(dp[idx][sum+total]!=-1)return dp[idx][sum+total];
+        int plus=solve(nums,idx+1,target,sum+nums[idx],dp,total);
+        int minus=solve(nums,idx+1,target,sum-nums[idx],dp,total);
+        return dp[idx][sum+total]=plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum=0;
         int count=0;
-        // vector<vector<int>>dp(nums.size(),vector<int>());
-        return solve(nums,0,target,sum);
+        int total=accumulate(nums.begin(),nums.end(),0);
+        vector<vector<int>>dp(nums.size(),vector<int>(2*total+1,-1));
+        return solve(nums,0,target,sum,dp,total);
     }
 };
