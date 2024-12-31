dab44a44c3bb403cb0809465cc09140645e111b7
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-01-01 00:46:34 +0530

    Time: 395 ms (24.20%) | Memory: 576.3 MB (6.61%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int curr,int prev,vector<vector<int>>& dp){
+    int solveMem(vector<int>& nums,int curr,int prev,vector<vector<int>>& dp){
         if(curr==nums.size()){
             return 0;
         }
@@ -8,13 +8,28 @@ public:
         //include
         int include=0;
         if(prev==-1 || nums[curr]>nums[prev]){
-            include=1+solve(nums,curr+1,curr,dp);
+            include=1+solveMem(nums,curr+1,curr,dp);
         }
-        int exclude=solve(nums,curr+1,prev,dp);
+        int exclude=solveMem(nums,curr+1,prev,dp);
         return dp[curr][prev+1]=max(include,exclude);
+    }
+
+    int solveTab(vector<int>& nums,int n){
+        vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1));
+        for(int curr=n-1;curr>=0;curr--){
+            for(int prev=curr-1;prev>=-1;prev--){
+                int include=0;
+                if(prev==-1 || nums[curr]>nums[prev]){
+                    include=1+dp[curr+1][curr+1];
+                }
+                int exclude=dp[curr+1][prev+1];
+                dp[curr][prev+1]=max(include,exclude);
+            }
+        }
+        return dp[0][0];
     }
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return solve(nums,0,-1,dp);
+        return solveTab(nums,nums.size());
     }
 };
