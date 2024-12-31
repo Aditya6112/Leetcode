2dc784f12c646d019dfef7b9108165e375bdef0b
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-01-01 01:09:20 +0530

    Time: 92 ms (40.87%) | Memory: 296.6 MB (6.86%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -48,8 +48,26 @@ public:
         }
         return currRow[0];
     }
+    //Optimal solution with Binary Search
+    int solveOptimal(vector<int>& nums,int n){
+        if(n==0){
+            return 0;
+        }
+        vector<int>ans;
+        ans.push_back(nums[0]);
+        for(int i=1;i<n;i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+            }else{
+                //just greater idx than nums[i]
+                int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[idx]=nums[i];
+            }
+        }
+        return ans.size();
+    }
     int lengthOfLIS(vector<int>& nums) {
         vector<vector<int>>dp(nums.size()+1,vector<int>(nums.size()+1,-1));
-        return solveTabSpaceOptimized(nums,nums.size());
+        return solveOptimal(nums,nums.size());
     }
 };
