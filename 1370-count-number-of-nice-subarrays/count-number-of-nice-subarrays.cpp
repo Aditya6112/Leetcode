a3f90b70dae7b5a0a71b5432fbeec77ce91635a1
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-01-03 01:13:08 +0530

    Time: 78 ms (20.04%) | Memory: 94.3 MB (14.01%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        //convert the array in 0,1 0 for even and 1 for odd
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]%2!=0){
+                nums[i]=1;
+            }else{
+                nums[i]=0;
+            }
+        }
+
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        int len=0;
+        int sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            int rem=sum-k;
+            if(mpp.find(rem)!=mpp.end()){
+                len+=mpp[rem];
+            }        
+            mpp[sum]++;
+        }
+        return len;
+    }
+};
