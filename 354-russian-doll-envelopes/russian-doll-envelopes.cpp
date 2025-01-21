b4465066aee2003c39b9713991a6c221b3373118
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-01-21 15:16:32 +0530

    Time: 85 ms (20.78%) | Memory: 94.6 MB (14.01%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.cpp
+++ 354-russian-doll-envelopes/russian-doll-envelopes.cpp
@@ -4,43 +4,39 @@ public:
         if(A[0]==B[0])return A[1]>B[1];
         return A[0]<B[0];
     }
-    //Gives MLE from this
-    int solveLIS(vector<int>heights,int n,int curr,int prev,vector<vector<int>>& dp){
-        if(curr==n)return 0;
-        if(dp[curr][prev+1]!=-1)return dp[curr][prev+1];
+    int lis(vector<int>& arr,int idx,int prev){
+        if(idx>=arr.size())return 0;
+        //include the number
         int include=0;
-        if(prev==-1 || heights[curr]>heights[prev]){
-            include=1+solveLIS(heights,n,curr+1,curr,dp);
+        if(prev==-1 || arr[prev]<arr[idx]){
+            include=1+lis(arr,idx+1,idx);
         }
-        int exclude=solveLIS(heights,n,curr+1,prev,dp);
-        return dp[curr][prev+1]=max(include,exclude);
+        //exclude
+        int exclude=lis(arr,idx+1,prev);
+        return max(include,exclude);
     }
-    
-    //Binary search
-    int solveOptimal(vector<int>& nums,int n){
-        if(n==0){
-            return 0;
-        }
-        vector<int>ans;
-        ans.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            if(nums[i]>ans.back()){
-                ans.push_back(nums[i]);
+    int dpWithBinarySearch(vector<int>& arr){
+        if(arr.size()==0)return 0;
+        vector<int>longest_increasing_length;
+        longest_increasing_length.push_back(arr[0]);
+        for(int i=1;i<arr.size();i++){
+            if(longest_increasing_length.back()<arr[i]){
+                longest_increasing_length.push_back(arr[i]);
             }else{
-                //just greater idx than nums[i]
-                int idx=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-                ans[idx]=nums[i];
+                //find just greater element than arr[i]
+                int idx=lower_bound(longest_increasing_length.begin(),longest_increasing_length.end(),arr[i])-longest_increasing_length.begin();
+                longest_increasing_length[idx]=arr[i];
             }
         }
-        return ans.size();
+        return longest_increasing_length.size();
     }
-
     int maxEnvelopes(vector<vector<int>>& envelopes) {
         sort(envelopes.begin(),envelopes.end(),cmp);
         vector<int>heights;
         for(auto env:envelopes){
             heights.push_back(env[1]);
         }
-        return solveOptimal(heights,heights.size());
+        // return lis(heights,0,-1);
+        return dpWithBinarySearch(heights);
     }
 };
