0395cbbb2bd8ed01f15a1b702bf2ae4663290a3e
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-03-04 11:37:38 +0530

    Time: 23 ms (78.88%) | Memory: 27.4 MB (66.04%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -15,10 +15,27 @@ public:
             return dp[n][m]=max(solve(text1,text2,n-1,m,dp),solve(text1,text2,n,m-1,dp));
         }
     }
+    //Tabulation method/Top dowm approach
+    int solveTopDown(string& text1, string& text2,int n,int m){
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
-        vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
-        return solve(text1,text2,n,m,dp);
+        // vector<vector<int>>dp(n+1,vector<int>(m+1,-1));
+        // return solve(text1,text2,n,m,dp);
+
+        return solveTopDown(text1,text2,n,m);
     }
 };
