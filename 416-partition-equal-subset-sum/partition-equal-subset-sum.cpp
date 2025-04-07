3d9674c1599af7637e863e875f3c1bbdfd871fe5
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-04-07 23:41:04 +0530

    Time: 40 ms (94.60%) | Memory: 101 MB (21.42%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-    //Recursion + Memoization
     bool subsetSum(vector<int>& nums,int n,int target,vector<vector<int>>& dp){
         if(target==0){
             return true;
@@ -17,44 +16,13 @@ public:
         }
         return dp[n][target]=subsetSum(nums,n-1,target,dp);
     }
-    //Top Down Approach
-    bool subsetSumII(vector<int>& nums,int n,int target){
-        vector<vector<bool>>dp(n+1,vector<bool>(target+1,false));
-        for(int i=0;i<n;i++){
-            for(int j=0;j<target;j++){
-                //when arr is empty means size is 0
-                if(i==0){
-                    dp[i][j]=false;
-                }
-                //when sum is 0 possible with empty subset always
-                if(j==0){
-                    dp[i][j]=true;
-                }
-            }
-        }
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=target;j++){
-                if(nums[i-1]<=j){
-                    dp[i][j]=dp[i-1][j-nums[i-1]] || dp[i-1][j];
-                }else{
-                    dp[i][j]=dp[i-1][j];
-                }
-            }
-        }
-        return dp[n][target];
-    }
     bool canPartition(vector<int>& nums) {
-        int total_sum=accumulate(nums.begin(),nums.end(),0);
-        if(total_sum%2!=0){
+        int n=nums.size();
+        int target=accumulate(nums.begin(),nums.end(),0);
+        if(target%2!=0){
             return false;
         }
-        int n=nums.size();
-
-        //Recursion + Memoization
-        // vector<vector<int>>dp(n+1,vector<int>((total_sum/2)+1,-1));
-        // return subsetSum(nums,n,total_sum/2,dp);
-
-        //Top Down Approach
-        return subsetSumII(nums,n,total_sum/2);
+        vector<vector<int>>dp(n+1,vector<int>((target/2)+1,-1));
+        return subsetSum(nums,n,target/2,dp);
     }
 };
