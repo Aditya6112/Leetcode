bff4a9f243033ba4615175289f9f1a1cf21af477
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-04-10 23:21:54 +0530

    Time: 50 ms (26.25%) | Memory: 54 MB (8.94%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,21 +1,24 @@
 class Solution {
 public:
-    int solve(vector<int>& coins,int i,int n,int amount,vector<vector<int>>& dp){
-        //we are counting coins if amount is 0 then req coins is also 0
-        if(amount==0)return 0;
-        if(i>=n)return 1e9;
-        if(dp[i][amount]!=-1)return dp[i][amount];
-        int take=1e9;
-        if(coins[i]<=amount){
-            take=1+solve(coins,i,n,amount-coins[i],dp);
+    int solve(vector<int>& coins, int n, int amount, vector<vector<int>>& dp) {
+        if (amount == 0)
+            return 0;
+        if (n <= 0)
+            return INT_MAX - 1;
+        if (dp[n][amount] != -1)
+            return dp[n][amount];
+        int take = INT_MAX - 1;
+        if (coins[n - 1] <= amount) {
+            take = 1 + solve(coins, n, amount - coins[n - 1], dp);
         }
-        int not_take=solve(coins,i+1,n,amount,dp);
-        return dp[i][amount]=min(take,not_take);
+        int not_take = solve(coins, n - 1, amount, dp);
+        return dp[n][amount] = min(take, not_take);
     }
+
     int coinChange(vector<int>& coins, int amount) {
-        int n=coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        int ans=solve(coins,0,n,amount,dp);
-        return ans>=1e9 ? -1 : ans;
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1, -1));
+        int ans = solve(coins, n, amount, dp);
+        return (ans == INT_MAX - 1) ? -1 : ans;
     }
 };
