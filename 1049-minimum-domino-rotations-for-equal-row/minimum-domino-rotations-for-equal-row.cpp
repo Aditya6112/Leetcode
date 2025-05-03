bdb633e4615ac443d8eca5bbbfeade9e51cc234d
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-05-04 01:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 115.4 MB (76.55%) - LeetSync

---
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        int n=tops.size();
+        int ans=INT_MAX;
+        for(int it=1;it<=6;it++){
+            int cnt1=0;
+            int cnt2=0;
+            for(int i=0;i<n;i++){
+                if(cnt1!=-1){
+                    if(tops[i]==it){
+                        // continue;
+                    }else if(bottoms[i]==it){
+                        cnt1++;
+                    }else{
+                        cnt1=-1;
+                    }
+                }
+                
+                if(cnt2!=-1){
+                    if(bottoms[i]==it){
+                        // continue;
+                    }else if(tops[i]==it){
+                        cnt2++;
+                    }else{
+                        cnt2=-1;
+                    }
+                }
+
+                if(cnt1==-1 && cnt2==-1)break;
+            }
+            if(cnt1!=-1)ans=min(ans,cnt1);
+            if(cnt2!=-1)ans=min(ans,cnt2);
+        }
+        return ans==INT_MAX ? -1 : ans;
+    }
+};
