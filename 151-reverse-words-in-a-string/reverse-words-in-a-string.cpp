34bcf19baf21eae0aae16e9e6b0039f3d7886455
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-08-15 02:15:53 +0530

    Time: 1 ms (54.18%) | Memory: 10.4 MB (68.03%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cpp
@@ -3,29 +3,26 @@ public:
     string reverseWords(string s) {
         string ans="";
         string temp="";
-        stack<char> st;
-        for(int i=s.size()-1 ;i>=0 ;i--){
+        for(int i=s.size()-1;i>=0;i--){
             if(s[i]!=' '){
-                st.push(s[i]);
-            }else if(s[i]==' ' && !st.empty()){
-                while(!st.empty()){
-                    temp+=st.top();
-                    st.pop();
-                }
-                ans+=temp;
-                ans+=' ';
+                temp+=s[i];
+            }else{
+                reverse(temp.begin(),temp.end());
+                if(temp!="")ans+=temp+' ';
+                else ans+=temp;
                 temp="";
             }
         }
-        while(!st.empty()){
-            temp+=st.top();
-            st.pop();
+        reverse(temp.begin(),temp.end());
+        ans+=temp+' ';
+        int i=ans.size()-1;
+        while(i>=0){
+            if(ans[i]==' '){
+                i--;
+            }else{
+                break;
+            }
         }
-        ans+=temp;
-        temp="";
-        if(ans[ans.size()-1]==' '){
-            ans.pop_back();
-        }
-        return ans;
+        return ans.substr(0,i+1);
     }
 };
