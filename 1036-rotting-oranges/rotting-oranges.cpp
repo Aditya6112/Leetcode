77f2bbc4e61d56aaff7369281719b759b26b44cc
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-10-05 00:19:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.9 MB (54.82%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
         queue<pair<pair<int,int>,int>>q;
-        vector<vector<int>>vis(grid.size(),vector<int>(grid[0].size(),0));
-        for (int i = 0; i < grid.size(); i++) {
-            for (int j = 0; j < grid[0].size(); j++) {
+        vector<vector<int>>vis(n,vector<int>(m,0));
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==2){
                     q.push({{i,j},0});
                     vis[i][j]=2;
@@ -12,9 +14,9 @@ public:
             }
         }
 
-        int dirx[] = {0, 1, 0, -1}; // Direction arrays for row
-        int diry[] = {1, 0, -1, 0}; // Direction arrays for column
-        int time = 0; // Count of rounds/steps needed for rotting
+        int dirx[]={0,1,0,-1};
+        int diry[]={1,0,-1,0};
+        int time=0;
 
         while(!q.empty()){
             int row=q.front().first.first;
@@ -26,16 +28,14 @@ public:
             for(int i=0;i<4;i++){
                 int nrow=row+dirx[i];
                 int ncol=col+diry[i];
-                if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() &&
-                    vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
-                        q.push({{nrow,ncol},t+1});
-                        vis[nrow][ncol]=2;
-                    }
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && grid[nrow][ncol]==1){
+                    q.push({{nrow,ncol},t+1});
+                    vis[nrow][ncol]=2;
+                }
             }
         }
-
-        for(int i=0;i<grid.size();i++){
-            for(int j=0;j<grid[0].size();j++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
                 if(grid[i][j]==1 && vis[i][j]!=2)return -1;
             }
         }
