d601ab7ce4ad2592babd49883612a6957a02afa0
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2025-12-08 00:17:58 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.72%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -7,22 +7,19 @@ public:
             int m=s+(e-s)/2;
             if(nums[m]==target){
                 return m;
-            }
-            //left part is sorted
-           if(nums[s]<=nums[m]){
+            }else if(nums[s]<=nums[m]){
                 if(nums[s]<=target && nums[m]>=target){
                     e=m-1;
                 }else{
                     s=m+1;
                 }
-           }
-           else{
+            }else{
                 if(nums[e]>=target && nums[m]<=target){
                     s=m+1;
                 }else{
                     e=m-1;
                 }
-           }
+            }
         }
         return -1;
     }
