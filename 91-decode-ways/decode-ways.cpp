fb3e9974d372c7974c48be2e9ae82b56990c935b
ADITYA RAJ <93546421+Aditya6112@users.noreply.github.com>
2026-01-07 00:31:05 +0530

    Time: 87 ms (5.01%) | Memory: 34.1 MB (5.43%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,39 +1,29 @@
 class Solution {
 public:
-    int solve(string s,int idx,int n,vector<int>& dp){
-        if(idx==n)return 1;
-        if(s[idx]=='0')return 0;
-        if(idx>=n)return 0;
+    int solve(string s,string str,int idx,unordered_set<string>& mp,vector<int>& dp){
+        if(idx==s.size()){
+            return 1;
+        }
+        if(idx>s.size()){
+            return 0;
+        }
         if(dp[idx]!=-1)return dp[idx];
-        int result=solve(s,idx+1,n,dp);
-        if(idx+1<n){
-            if((s[idx]=='1') || (s[idx]=='2' && s[idx+1]<='6')){
-                result+=solve(s,idx+2,n,dp);
+        int sum=0;
+        for(int len=1;len<=s.size();len++){
+            str=s.substr(idx,len);
+            if(mp.find(str)!=mp.end()){
+                sum+=solve(s,str,idx+len,mp,dp);
             }
         }
-        return dp[idx]=result;
-    }
-    int bottomUp(string s,int n){
-        vector<int> dp(n+1,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0'){
-                dp[i]=0;
-            }else{
-                dp[i]=dp[i+1];
-                if(i+1<n){
-                    if((s[i]=='1') || (s[i]=='2' && s[i+1]<='6')){
-                        dp[i]+=dp[i+2];
-                    }
-                }
-            }
-        }
-        return dp[0];
+        return dp[idx]=sum;
     }
     int numDecodings(string s) {
-        int n=s.size();
-        int cnt=0;
-        vector<int>dp(n+1,-1);
-        return bottomUp(s,n);
+        unordered_set<string>mp;
+        string str="";
+        vector<int>dp(s.size()+1,-1);
+        for(int i=1;i<=26;i++){
+            mp.insert(to_string(i));
+        }
+        return solve(s,str,0,mp,dp);
     }
 };
